4dc562eb6068f2f0067bd7849daf5ffe9bd23087
Milson António <milsinho.milsinho@live.com>
2021-07-30 01:55:17 +0200

    dht11 funcionando

--- ESP8266 code/temperatureandhumidity/temperatureandhumidity.ino
+++ ESP8266 code/temperatureandhumidity/temperatureandhumidity.ino
@@ -1,17 +1,54 @@
 
-int led = 13;
+#include "DHT.h"
+
+#define DHTTYPE DHT11
+
+// DHT Sensor
+uint8_t DHTPin = 13; 
+               
+// Initialize DHT sensor.
+DHT dht(DHTPin, DHTTYPE);                
+
+float Temperature;
+float Humidity;
+
+//int greenLed = 13;
+int redLed = 12;
 void setup() {
-  pinMode(led, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
+
+ Serial.begin(9600);
+
+ Serial.println("DHTxx test!");
+
+ pinMode(DHTPin, INPUT);
+
+ dht.begin();  
+
+  
+  //pinMode(greenLed, OUTPUT);     
+  //pinMode(redLed, OUTPUT); 
 }
 
 
 
-// the loop function runs over and over again forever
 void loop() {
-  digitalWrite(led, LOW);   // Turn the LED on (Note that LOW is the voltage level
-  // but actually the LED is on; this is because
-  // it is active low on the ESP-01)
-  delay(1000);                      // Wait for a second
-  digitalWrite(led, HIGH);  // Turn the LED off by making the voltage HIGH
-  delay(2000);                      // Wait for two seconds (to demonstrate the active low LED)
+
+  Temperature = dht.readTemperature();
+  Humidity = dht.readHumidity(); 
+
+   Serial.println(Temperature);
+   Serial.println(Humidity);
+
+   delay(2000);                      
+
+
+
+
+  
+  //digitalWrite(greenLed, LOW);  
+  //digitalWrite(redLed, HIGH); 
+  //delay(1000);                      
+  //digitalWrite(greenLed, HIGH);  
+  //digitalWrite(redLed, LOW); 
+  //delay(2000);                      
 }
